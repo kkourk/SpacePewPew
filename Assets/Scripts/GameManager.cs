5cd515dd20cf5441f3400803ff4c957cb9867be0
Miroslav <msokol15@student.aau.dk>
2018-03-06 12:37:08 +0100

    Added Prefab, Updated Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public static GameManager instance = null;
+
+	//Awake is always called before any Start functions
+	void Awake()
+	{
+		//Check if instance already exists
+		if (instance == null)
+
+			//if not, set instance to this
+			instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy (gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad (gameObject);
 	}
 }
