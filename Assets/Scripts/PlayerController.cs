b2f0418efd27fc7589b5b79c5c230c309e9bf2ec
Miroslav <msokol15@student.aau.dk>
2018-03-06 15:09:14 +0100

    Shooting
    Something works

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject fireballPrefab;
+    public Transform fireballSpawn;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+        //float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        //transform.Rotate(0, speed, 0);
+    }
+
+
+    public void Shoot()
+    {
+        
+
+        float deadzone = 0.7f;
+        Vector2 stickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (stickInput.magnitude > deadzone)
+        {
+            GameObject fire = Instantiate(fireballPrefab, fireballSpawn.position, fireballSpawn.rotation);
+            fire.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * 6;
+            Destroy(fire, 2.0f);
+        }
+
+        
+
+        Debug.Log("Fire");
+    }
+}
